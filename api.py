519b6ac1d03b3f2d8c35222566ef607ddb7b98a2
nankotsukan <bmq1040@mail4.doshisha.ac.jp>
2019-02-19 19:46:32 +0900

    にゃーん

--- api.py
+++ api.py
@@ -3,15 +3,21 @@ import json
 
 def search(word="たこやき"):
     hoge = []
-    for i in range(1, 100000):
-        try:
-            with request.urlopen(f"https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=a8894c3975e74a3cba1b20e617663e8c&hit_per_page=100&offset_page={i}&freeword={parse.quote(word)}") as res:
-                html = res.read().decode("utf-8")
-        except:
-            break
-        a = json.loads(html)
-        for x in a["rest"]:
-            hoge.append({"id":x["id"], "url": x["url"], "latitude": x["latitude"], "longitude": x["longitude"]})
+    with request.urlopen("https://api.gnavi.co.jp/master/GAreaLargeSearchAPI/v3/?keyid=a8894c3975e74a3cba1b20e617663e8c") as res:
+        html = res.read().decode("utf-8")
+        for x in json.loads(html)["garea_large"]:
+            area=x["areacode_l"]
+            fuga = []
+            for i in range(1, 100000):
+                try:
+                    with request.urlopen(f"https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=a8894c3975e74a3cba1b20e617663e8c&hit_per_page=100&areacode_l={area}&offset_page={i}&freeword={parse.quote(word)}") as res:
+                        html = res.read().decode("utf-8")
+                except:
+                    break
+                a = json.loads(html)
+                for x in a["rest"]:
+                    fuga.append({"id":x["id"], "url": x["url"], "latitude": x["latitude"], "longitude": x["longitude"]})
+            hoge.extend(fuga)
     return hoge
 
 
