1e4a5dcc013e878c84c24536c09e189016406899
石川幹晴 <k.ishikw@gmail.com>
2019-02-19 19:23:37 +0900

    一番近い駅の取得

---
+++ station.py
@@ -0,0 +1,45 @@
+import math
+import time
+import glob
+
+class station_search:
+    def __init__(self, latitude, longitude):
+        self.la = latitude
+        self.lo = longitude
+
+    def min_dist_station( self ):
+        station_data = self.station_list()
+
+        for i in range( 0, len( station_data ) ):
+            station_data[i]["dist"] = self.min_dist( station_data[i]["point"][0], station_data[i]["point"][1] )
+
+        station_data.sort( key = lambda x: x["dist"] )
+        print(station_data[0]["name"])
+        
+    def min_dist( self, latitude, longitude ):
+        dist = math.sqrt( math.pow( ( self.la - latitude), 2 ) + math.pow( ( self.lo - longitude ), 2 ) )
+        earth_dist = 40000.0 * 1000 / 360#経度、緯度１度あたりのmの距離
+        min_dist = dist * earth_dist
+        return min_dist
+
+    def station_list( self ):
+        file_list = glob.glob("*.txt")
+        station_main = []
+        
+        for i in range( 0, len(file_list) ):
+            f = open( file_list[i] )
+            station_data = f.readlines()
+            
+            for r in range( 0, len( station_data ) ):
+                station_place = {}
+                station_data[r] = station_data[r].replace("\n", "")
+                station = station_data[r].split(",")
+                x_y = ( float( station[1] ), float( station[2] ) )
+                station_place["name"] = station[0]
+                station_place["point"] = x_y
+                station_main.append( station_place )
+
+        return station_main
+
+s = station_search( 34.981367, 135.961507 )
+s.min_dist_station()
--- time.py
+++ time.py
@@ -7,7 +7,7 @@ def execute( current_latitude, current_longitude ):
     for i in range( 0, len( shop_list )):
         shop_list[i]["dist"] = min_dist( float( shop_list[i]["latitude"] ),  float( shop_list[i]["longitude"] ), current_latitude, current_longitude)
     shop_list.sort( key = lambda x: x["dist"])
-    print(shop_list)
+    print(shop_list[0])
     
 def min_dist( latitude1, longitude1, latitude2, longitude2 ):
     dist = math.sqrt( pow( ( latitude1 - latitude2), 2 ) + math.pow( (longitude1 - longitude2), 2 ) )
@@ -15,10 +15,4 @@ def min_dist( latitude1, longitude1, latitude2, longitude2 ):
     min_dist = dist * earth_dist
     return min_dist
 
-def min_shop():
-
-    test_list.sort( key = lambda x: x["距離"])
-    print(test_list)
-
-execute( 34.981367, 135.961507 )
-    
+execute( 34.981367, 135.961507 )    
