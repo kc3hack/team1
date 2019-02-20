7635b69a5c36565fb6e4d5a059cba8ff035546d8
石川幹晴 <k.ishikw@gmail.com>
2019-02-20 11:49:10 +0900

    url追加

--- st_main.py
+++ st_main.py
@@ -14,15 +14,16 @@ def execute( latitude, longitude , food_name, per_time):
     my_near_station = station.min_dist_station( latitude, longitude )#自分に一番近い駅の名前を取得
     ros = root.root_seach( my_near_station )
     
-    for i in range( 0, 5 ):
+    for i in range( 0, per_time ):
         station_ = {}
         arrival_name = station.min_dist_station( float( shop[count][i]["latitude"] ), float( shop[count][i]["longitude"] ) )
         departure_time, arrival_time, spend_time = ros.time( arrival_name )
         station_["spend"] = spend_time
         station_["departure"] = my_near_station
         station_["arrival"] = arrival_name
+        station_["url"] = shop[count][i]["url"]
         station_list.append( station_ )
-
+        
     print( station_list )
     return station_list
-execute( 34.982353, 135.961761, "たこ焼き", 30)
+execute( 34.982353, 135.961761, "たこ焼き", 5)
