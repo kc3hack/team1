8d19650aaab880162108a34233855e8ac957aba4
nankotsukan <bmq1040@mail4.doshisha.ac.jp>
2019-02-20 10:37:05 +0900

    おはようございます

--- api.py
+++ api.py
@@ -1,25 +1,35 @@
 from urllib import request, parse
 import json
+from concurrent.futures import ThreadPoolExecutor
+import itertools
+import pickle
 
-def search(word="たこやき"):
-    hoge = []
-    with request.urlopen("https://api.gnavi.co.jp/master/GAreaLargeSearchAPI/v3/?keyid=a8894c3975e74a3cba1b20e617663e8c") as res:
+API_KEY = "041bf926060de0bdcbe537265b4c78ea"
+
+def search(word="たこ焼き"):
+    def foo(area):
+        fuga = []
+        for i in range(1, 100000):
+            try:
+                with request.urlopen(f"https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid={API_KEY}&hit_per_page=100&areacode_l={area}&offset_page={i}&freeword={parse.quote(word)}&freeword_condition=2") as res:
+                    # ','.join(word)
+                    html = res.read().decode("utf-8")
+            except Exception as e:
+                break
+            a = json.loads(html)
+            for x in a["rest"]:
+                fuga.append({"id":x["id"], "url": x["url"], "latitude": x["latitude"], "longitude": x["longitude"]})
+        return fuga
+    pool = ThreadPoolExecutor(max_workers=32)
+    with request.urlopen(f"https://api.gnavi.co.jp/master/GAreaLargeSearchAPI/v3/?keyid={API_KEY}") as res:
         html = res.read().decode("utf-8")
-        for x in json.loads(html)["garea_large"]:
-            area=x["areacode_l"]
-            fuga = []
-            for i in range(1, 100000):
-                try:
-                    with request.urlopen(f"https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=a8894c3975e74a3cba1b20e617663e8c&hit_per_page=100&areacode_l={area}&offset_page={i}&freeword={parse.quote(word)}") as res:
-                        html = res.read().decode("utf-8")
-                except:
-                    break
-                a = json.loads(html)
-                for x in a["rest"]:
-                    fuga.append({"id":x["id"], "url": x["url"], "latitude": x["latitude"], "longitude": x["longitude"]})
-            hoge.extend(fuga)
+    result = pool.map(foo, [x["areacode_l"] for x in json.loads(html)["garea_large"]])
+    hoge = list(itertools.chain.from_iterable(result))
     return hoge
 
 
 if __name__ == '__main__':
-    print(len(search()))
+    with open('takoyaki.pickle', mode='wb') as f:
+        pickle.dump(search("たこ焼き"), f)
+    with open('okonomiyaki.pickle', mode='wb') as f:
+        pickle.dump(search("お好み焼き"), f)
