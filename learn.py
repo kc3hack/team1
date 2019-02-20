a128b255ae9cca531ef2cee439f802b4ad48deed
nankotsukan <bmq1040@mail4.doshisha.ac.jp>
2019-02-20 15:02:02 +0900

    転移学習の実装

---
+++ learn.py
@@ -0,0 +1,22 @@
+from chainer.links import ResNet152Layers
+import numpy as np
+from sklearn import datasets
+import numpy as np
+from sklearn import datasets
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+def learn(image_x = [], image_y = [], vec = [], test_num = 5):
+    model = ResNet152Layers()
+
+    for x in image_x[:-test_num] + image_y[:-test_num]:
+        vec.append(model(np.zeros((3,3,224,224), dtype=np.float32))["prob"])
+
+    vec = np.array(vec)
+    y_train = np.hstack([np.zeros(len(image_x)-test_num), np.ones(len(image_y)-test_num)])
+
+    svm = SVC(kernel='linear', random_state=None)
+    svm.fit(vec, y_train)
+
+    acc = accuracy_score(y_train, svm.predict(vec))
+    print('acc： %d%%' % acc*100)
