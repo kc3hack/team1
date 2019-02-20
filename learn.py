6fa23e0a1eae7a208f031d9da9e46a8f8c213670
nankotsukan <bmq1040@mail4.doshisha.ac.jp>
2019-02-20 16:17:30 +0900

    あああああああ

--- learn.py
+++ learn.py
@@ -13,9 +13,9 @@ def make_vec(image_x = [], image_y = [], test_num = 5):
     vec_0 = []
     vec_1 = []
     for x in image_x:
-        vec_0.append(model(np.zeros((3,3,224,224), dtype=np.float32))["prob"])
+        vec_0.append(model(np.zeros((3,3,224,224), dtype=np.float32))["prob"].data)
     for x in image_y:
-        vec_1.append(model(np.zeros((3,3,224,224), dtype=np.float32))["prob"])
+        vec_1.append(model(np.zeros((3,3,224,224), dtype=np.float32))["prob"].data)
         
     x_train = vec_0[:-test_num]+vec_1[:-test_num]
     y_train = np.hstack([np.zeros(len(image_x)-test_num), np.ones(len(image_y)-test_num)])
@@ -32,9 +32,14 @@ def learn(x_train, y_train, x_test, y_test, kernel='linear'):
     print('acc： %d%%' % acc*100)
 
 def main():
-    x_0, x_1 = learn_data.execute()
-    hoge = make_vec(x_0, x_1)
-    np.savez("vector.npz", *hoge)
+    # x_0, x_1 = learn_data.execute()
+    # hoge = make_vec(x_0, x_1)
+    # np.savez("vector.npz", *hoge)
+    fuga = np.load("vector.npz")
+    hoge = [fuga[x] for x in fuga.files]
+    for x in hoge:
+        print(x.shape)
+        # print(np.linalg.norm(x, axis=))
     learn(*hoge)
 
 
