bc393c2c2fe7b4ac294b5e5a46b4cf6300074432
nankotsukan <bmq1040@mail4.doshisha.ac.jp>
2019-02-20 17:12:42 +0900

    ﾆｬｰﾝ

--- learn.py
+++ learn.py
@@ -6,6 +6,7 @@ from sklearn import datasets
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import learn_data
+import chainer.computational_graph as c
 
 def make_vec(image_x = [], image_y = [], test_num = 5):
     model = ResNet152Layers()
@@ -13,9 +14,9 @@ def make_vec(image_x = [], image_y = [], test_num = 5):
     vec_0 = []
     vec_1 = []
     for x in image_x:
-        vec_0.append(model(np.zeros((3,3,224,224), dtype=np.float32))["prob"].data)
+        vec_0.append(model(x.reshape(1, *x.shape))["prob"].data[0])
     for x in image_y:
-        vec_1.append(model(np.zeros((3,3,224,224), dtype=np.float32))["prob"].data)
+        vec_1.append(model(x.reshape(1, *x.shape))["prob"].data[0])
         
     x_train = vec_0[:-test_num]+vec_1[:-test_num]
     y_train = np.hstack([np.zeros(len(image_x)-test_num), np.ones(len(image_y)-test_num)])
@@ -24,22 +25,23 @@ def make_vec(image_x = [], image_y = [], test_num = 5):
 
     return x_train, y_train, x_test, y_test
 
-def learn(x_train, y_train, x_test, y_test, kernel='linear'):
+def learn(x_train, y_train, x_test, y_test, kernel='rbf'):
     svm = SVC(kernel=kernel, random_state=None)
     svm.fit(x_train, y_train)
-
+    print(svm.predict(x_test))
+    print(y_test)
     acc = accuracy_score(y_test, svm.predict(x_test))
-    print('acc： %d%%' % acc*100)
+    # print('acc： %d%%' % acc*100)
 
 def main():
     # x_0, x_1 = learn_data.execute()
     # hoge = make_vec(x_0, x_1)
-    # np.savez("vector.npz", *hoge)
+    # np.savez("vector1.npz", *hoge)
     fuga = np.load("vector.npz")
     hoge = [fuga[x] for x in fuga.files]
-    for x in hoge:
-        print(x.shape)
-        # print(np.linalg.norm(x, axis=))
+    # for x in hoge:
+    #     print(x)
+    #     print(np.linalg.norm(x))
     learn(*hoge)
 
 
--- learn_data.py
+++ learn_data.py
@@ -6,9 +6,6 @@ def execute():
     takoyaki_list = glob.glob( "takoyaki/*.jpeg" )
     okonomiyaki_list = glob.glob( "okonomiyaki/*jpg" )
     size = ( 224, 224 )
-    x_data = np.zeros(( 1, 3, 224, 224 ))
-    x_data_instance = np.zeros(( 1, 3, 224, 224 ))
-    answer_data = np.array( [] )
     
     x_0 = []
     for i in range( 0, len( takoyaki_list ) ):
@@ -16,8 +13,7 @@ def execute():
         new_img = cv2.resize( img, size )
         new_img = new_img.transpose()
         x_0.append(new_img)
-    x_0 = np.array(x_0)
-
+    x_0 = np.array(x_0, dtype=np.float32)
 
     x_1 = []
     for i in range( 1, len( okonomiyaki_list ) ):
@@ -25,7 +21,6 @@ def execute():
         new_img = cv2.resize( img, size )
         new_img = new_img.transpose()
         x_1.append(new_img)
-    x_1 = np.array(x_1)
+    x_1 = np.array(x_1, dtype=np.float32)
 
-    x_data = x_data.astype( np.float32 )
     return x_0, x_1
