60e2761750fc436ee83adaaa6ecd1c936049a6da
ruritoblogger <rurito.blog@gmail.com>
2019-02-21 13:00:55 +0900

    fixed

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     
-    # json5
+    # json6
     tmp = json.loads(body)
 
     # handle webhook bodya
