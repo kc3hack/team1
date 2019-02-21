763060f663d455ea7ac61ac73efc79da4f62e313
ruritoblogger <rurito.blog@gmail.com>
2019-02-21 11:44:56 +0900

    aptfile

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     
-    # json
+    # json2
     tmp = json.loads(body)
 
     # handle webhook bodya
